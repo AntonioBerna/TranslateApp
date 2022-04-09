83de6f8e6ae0d785c1bc2a7fd5c9e27a5dc7ecc6
AntonioBerna <asukeasukai12@gmail.com>
2022-04-09 18:18:07 +0200

    Update Code Design 🤣

--- app.py
+++ app.py
@@ -17,21 +17,16 @@ class TranslateApp(tk.Tk):
 
         self.original_text = tk.Text(self, height=10, width=40, font=("Cascadia Code", 20), highlightthickness=1, borderwidth=0)
         self.original_text.grid(row=0, column=0, pady=20, padx=10)
-
         self.translate_button = tk.Button(self, text="Translate!", font=("Cascadia Code", 20), command=self.translate)
         self.translate_button.grid(row=0, column=1, padx=10)
-
         self.translated_text = tk.Text(self, height=10, width=40, font=("Cascadia Code", 20), highlightthickness=1, borderwidth=0)
         self.translated_text.grid(row=0, column=2, pady=20, padx=10)
-
         self.original_combo = ttk.Combobox(self, width=50, value=self.language_list)
         self.original_combo.current(45) # Italian
         self.original_combo.grid(row=1, column=0)
-
         self.translated_combo = ttk.Combobox(self, width=50, value=self.language_list)
         self.translated_combo.current(21) # English
         self.translated_combo.grid(row=1, column=2)
-
         self.clear_button = tk.Button(self, text="Clear", font=("Cascadia Code", 20), command=self.clear)
         self.clear_button.grid(row=2, column=1)
     
